LineageOS/android_device_generic_common

Compile guard aosp apns conf in gsi product
Conflicts with Lineage's BUILD_PREBUILT apns and can't be fixed by include order.

Change-Id: Idb7dca607243fefffe8c16de019a75f26d275ddf

gsi_product.mk
@@ -34,5 +34,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.config.notification_sound?=pixiedust.ogg \
     ro.com.android.dataroaming?=true \
 
+ifeq ($(LINEAGE_BUILD),)
 PRODUCT_COPY_FILES += \
     device/sample/etc/apns-full-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
+endif

LMODroid-Devices/device_generic_common

Conflicts with LMODroid's BUILD_PREBUILT apns and can't be fixed by include order.
Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>
(cherry picked from commit 14e65afac3594f550121b144df2283c0d480f780)

gsi_product.mk
@@ -34,5 +34,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.config.notification_sound?=pixiedust.ogg \
     ro.com.android.dataroaming?=true \
 
+ifeq ($(LMODROID_BUILD),)
 PRODUCT_COPY_FILES += \
     device/sample/etc/apns-full-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
+endif